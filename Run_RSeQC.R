65facc9ec96dcfd2decd48b6caaade12ad00034a
rsteinfe1 <rsteinfe@fredhutch.org>
2023-10-24 14:22:59 -0700

    Added laneids

--- Run_RSeQC.R
+++ Run_RSeQC.R
@@ -4,5 +4,7 @@ library(ggplot2)
 ## RSeQC
 ## Please run this via bash script (e.g. run_all.sh)
 
+input.dir = '/fh/fast/peters_u/GECCO_Working/robert_working/Projects/TRPCD_RNASeqQC/input/'
 
+laneids = list.files(input.dir)[c(2,5,6,7,8)]
 
