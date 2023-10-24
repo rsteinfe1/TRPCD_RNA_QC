a1247ef938c8afb5c3a2db5377f462b0d376c494
rsteinfe1 <rsteinfe@fredhutch.org>
2023-10-24 16:35:34 -0700

    "Added bam names and tools and reference build"

--- Run_RSeQC.R
+++ Run_RSeQC.R
@@ -1,10 +1,23 @@
 library(ggplot2)
 
-## script to run
-## RSeQC
+## Wrapper to run
+## RSeQC scripts
 ## Please run this via bash script (e.g. run_all.sh)
+## To make sure all modules are loaded
 
 input.dir = '/fh/fast/peters_u/GECCO_Working/robert_working/Projects/TRPCD_RNASeqQC/input/'
+laneids = list.files(input.dir, full.names=T)[c(2,5,6,7,8)]
+bamsuf = '/Analysis/STAR/BAM/'
 
-laneids = list.files(input.dir)[c(2,5,6,7,8)]
+bams = list.files(paste0(laneids, bamsuf), pattern=".bam$", full.names=T)
+
+rseqc = paste0('/app/software/RSeQC/5.0.1-foss-2021b/bin/',
+        c("tin.py", "RNA_fragment_size.py", "inner_distance.py",
+          "infer_experiment.py", "junction_saturation.py",
+          "read_GC.py", "RPKM_saturation.py"))
+
+#Unfortunately, every tool needs its own config
+#Source: https://sourceforge.net/projects/rseqc/files/BED/Human_Homo_sapiens/
+#Build hg38
+
 
